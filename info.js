d2aa2dd4b720ed1bec90d5978c4d429f16a5b19d
Taylor Williams <williamstm96@gmail.com>
2020-02-27 21:47:17 -0700

    Adding files to store data

---
+++ info.js
@@ -0,0 +1,18 @@
+const resourceItemsData = [
+  {
+    "name": "5 Gallon Water Jug",
+    "href": "https://www.uline.com/Product/Detail/S-19412/Pails/Closed-Head-Poly-Pail-5-Gallon-Blue?pricode=WB9066&gadtype=pla&id=S-19412&gclid=EAIaIQobChMI-IXBpLLz5wIVk8pkCh18Jg86EAQYAyABEgKkavD_BwE&gclsrc=aw.ds",
+    "price": 20.00
+  },
+  {
+    "name": "1 Gallon Water Jug",
+    "href": "https://www.uline.com/Product/Detail/S-19412/Pails/Closed-Head-Poly-Pail-5-Gallon-Blue?pricode=WB9066&gadtype=pla&id=S-19412&gclid=EAIaIQobChMI-IXBpLLz5wIVk8pkCh18Jg86EAQYAyABEgKkavD_BwE&gclsrc=aw.ds",
+    "price": 5.00
+  }
+];
+
+const resourceInformationData = [
+  {
+
+  }
+];
