29188ebc54e8ac230a3bcabcd1e858f5da6cfe6c
Taylor Williams <williamstm96@gmail.com>
2020-02-29 10:56:18 -0700

    Fixing href

--- info.js
+++ info.js
@@ -1,8 +1,8 @@
 const resourceItemsData = [
   {
-    "name": "5 Gallon Water Jug",
-    "href": "https://www.uline.com/Product/Detail/S-19412/Pails/Closed-Head-Poly-Pail-5-Gallon-Blue?pricode=WB9066&gadtype=pla&id=S-19412&gclid=EAIaIQobChMI-IXBpLLz5wIVk8pkCh18Jg86EAQYAyABEgKkavD_BwE&gclsrc=aw.ds",
-    "price": 20.00
+    "name": "7 Gallon Water Jug",
+    "href": "https://www.amazon.com/Reliance-Products-Aqua-Tainer-Gallon-Container/dp/B001QC31G6?ref_=fsclp_pl_dp_2",
+    "price": 15.00
   },
   {
     "name": "1 Gallon Water Jug",
