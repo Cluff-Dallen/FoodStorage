7f158708defc326b4ae30fa36e5adf7e768c86d6
Taylor Williams <williamstm96@gmail.com>
2020-03-02 20:44:48 -0700

    working

--- info.js
+++ info.js
@@ -3,7 +3,39 @@ const resourceItemsData = [
     "name": "7 Gallon Water Jug",
     "href": "https://www.amazon.com/Reliance-Products-Aqua-Tainer-Gallon-Container/dp/B001QC31G6?ref_=fsclp_pl_dp_2",
     "imgSrc": "https://images-na.ssl-images-amazon.com/images/I/51eAFKbEKoL._AC_.jpg",
-    
+    "price": 15.00
+  },
+  {
+    "name": "1 Gallon Water Jug",
+    "href": "https://www.uline.com/Product/Detail/S-19412/Pails/Closed-Head-Poly-Pail-5-Gallon-Blue?pricode=WB9066&gadtype=pla&id=S-19412&gclid=EAIaIQobChMI-IXBpLLz5wIVk8pkCh18Jg86EAQYAyABEgKkavD_BwE&gclsrc=aw.ds",
+    "price": 5.00
+  },
+  {
+    "name": "7 Gallon Water Jug",
+    "href": "https://www.amazon.com/Reliance-Products-Aqua-Tainer-Gallon-Container/dp/B001QC31G6?ref_=fsclp_pl_dp_2",
+    "imgSrc": "https://images-na.ssl-images-amazon.com/images/I/51eAFKbEKoL._AC_.jpg",
+    "price": 15.00
+  },
+  {
+    "name": "1 Gallon Water Jug",
+    "href": "https://www.uline.com/Product/Detail/S-19412/Pails/Closed-Head-Poly-Pail-5-Gallon-Blue?pricode=WB9066&gadtype=pla&id=S-19412&gclid=EAIaIQobChMI-IXBpLLz5wIVk8pkCh18Jg86EAQYAyABEgKkavD_BwE&gclsrc=aw.ds",
+    "price": 5.00
+  },
+  {
+    "name": "7 Gallon Water Jug",
+    "href": "https://www.amazon.com/Reliance-Products-Aqua-Tainer-Gallon-Container/dp/B001QC31G6?ref_=fsclp_pl_dp_2",
+    "imgSrc": "https://images-na.ssl-images-amazon.com/images/I/51eAFKbEKoL._AC_.jpg",
+    "price": 15.00
+  },
+  {
+    "name": "1 Gallon Water Jug",
+    "href": "https://www.uline.com/Product/Detail/S-19412/Pails/Closed-Head-Poly-Pail-5-Gallon-Blue?pricode=WB9066&gadtype=pla&id=S-19412&gclid=EAIaIQobChMI-IXBpLLz5wIVk8pkCh18Jg86EAQYAyABEgKkavD_BwE&gclsrc=aw.ds",
+    "price": 5.00
+  },
+  {
+    "name": "7 Gallon Water Jug",
+    "href": "https://www.amazon.com/Reliance-Products-Aqua-Tainer-Gallon-Container/dp/B001QC31G6?ref_=fsclp_pl_dp_2",
+    "imgSrc": "https://images-na.ssl-images-amazon.com/images/I/51eAFKbEKoL._AC_.jpg",
     "price": 15.00
   },
   {
